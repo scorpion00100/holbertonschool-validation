f21e22d78ecb80d6ec04d2465e54b5be31a9646a
scorpion00100 <danbetheliryivuze@gmail.com>
2023-05-10 01:37:01 +0200

    update setup

--- module2_task4/setup.sh
+++ module2_task4/setup.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-cd /app
-git clone https://github.com/scorpion00100/holbertonschool-validation
-cd holbertonschool-validation/module1_task4/
-make build
-mv ./dist ../../
-exit
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
