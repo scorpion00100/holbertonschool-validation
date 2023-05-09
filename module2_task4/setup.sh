7f0462579390196edcb2480059d360b3fa5a1250
scorpion00100 <danbetheliryivuze@gmail.com>
2023-05-09 17:06:42 +0200

    update setup for generate web site

--- module2_task4/setup.sh
+++ module2_task4/setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 # inqtall a good version of hugo
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
-
-#generate go-hugo website
-make build
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
