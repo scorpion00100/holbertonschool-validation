eb55a65e019d9046e425ec4980a6a78d386c1f10
scorpion00100 <danbetheliryivuze@gmail.com>
2023-05-16 00:42:16 +0200

    config and update

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # installing good version of hugo
-apt-get update && apt-get install -y make wget
+sudo apt-get update && apt-get install -y make wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz
-
-# Install markdownlint and zip tools
-npm install -g markdownlint-cli
-npm install -g markdown-link-check
+sudo mv hugo /usr/local/bin/
+sudo rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
