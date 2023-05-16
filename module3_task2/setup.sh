94284128d8db62169e45ddd2a4fbab22efa8fa42
Dan Bethel IRYIVUZE <101734714+scorpion00100@users.noreply.github.com>
2023-05-16 02:14:37 +0200

    Update setup.sh

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y hugo make git wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_0.109.0_Linux-64bit.tar.gz
-tar -xf hugo_0.109.0_Linux-64bit.tar.gz 
-sudo mv ./hugo/usr/bin/
-sudo rm hugo_0.109.0_Linux-64bit.tar.gz
-sudo npm install -g markdownlint-cli
-sudo npm install -g markdown-link-check
+
+# Install appropriate version of hugo to be run in docker container 'ubuntu 18.04'
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+
+# Install markdownlint and zip tools
+apt-get install zip -y
+npm install -g markdownlint-cli -y
+
+# Generate a Go-Hugo website
+make build
