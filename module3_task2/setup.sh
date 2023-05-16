0ea10e38146700a62d80afe28d3607afaca0323e
scorpion00100 <danbetheliryivuze@gmail.com>
2023-05-16 02:10:44 +0200

    update all files

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt-get update && sudo apt-get install -y hugo make git wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_0.109.0_Linux-64bit.tar.gz
 tar -xf hugo_0.109.0_Linux-64bit.tar.gz 
