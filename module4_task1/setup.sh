7d486b1bd0f1082c93dba7d9fef2ea33cdd2175a
scorpion00100 <danbetheliryivuze@gmail.com>
2023-05-17 14:15:24 +0200

    update config setup

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Install appropriate version of hugo to be run in docker container 'ubuntu 22.04'
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
-tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
-mv hugo /usr/local/bin/
-rm hugo_extended_0.109.0_Linux-64bit.tar.gz || true
+#apt-get update && apt-get install -y make wget
+#wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+#tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
+#mv hugo /usr/local/bin/
+#rm hugo_extended_0.109.0_Linux-64bit.tar.gz || true
 
 # Install markdownlint and zip tools
-apt-get install zip -y
-npm install -g markdownlint-cli -y
+#apt-get install zip -y
+#npm install -g markdownlint-cli -y
 
 # Generate a Go-Hugo website
-make build
+#make build
 
 # Uninstall go
 sudo apt-get remove golang-go
